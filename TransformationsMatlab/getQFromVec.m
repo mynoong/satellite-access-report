b8867b43b21a307b3af16abda4d6676283853925
Minwoong Choi <minwoong@usc.edu>
2023-09-12 10:02:52 -0700

    Update and rename qFromVector.m to getQFromVec.m

---
+++ TransformationsMatlab/getQFromVec.m
@@ -0,0 +1,13 @@
+function q = getQFromVec(v)
+%getQFromVec transforms vector v into the quaternion with vector part v 
+%and scalar part 0
+
+if length(v) == 4
+  q = v; % v is alreay a quaternion
+elseif length(v) == 3
+  q = [v(1), v(2), v(3), 0];
+else
+  disp('[getQFromVec] v cannot be transformed into quaternion format!')
+  disp(v)
+  
+end
