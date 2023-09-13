bbfa097efcd9c51fbbbefdebbdf9c4be70951508
Minwoong Choi <minwoong@usc.edu>
2023-09-12 17:06:20 -0700

    Edit localNEU.m but not fully updated

---
+++ TransformationsMatlab/localNEU.m
@@ -0,0 +1,21 @@
+function [N, E, U] = localNEU(Lo, La)
+%localNEU finds the local north, east and up vectors (in ECEF)
+%at a location on Earth's surface
+
+if nargin < 2
+% Location of Seoul or N Seoul Tower
+  Lo = 126.9882;
+  La = 37.5512;
+end
+
+N0 = [0, 0, 1];
+E0 = [0, 1, 0];
+U0 = [1, 0, 0];
+
+q = qprod( qrotationZ(Lo), qrotationY(-La) );
+
+N = qrotate( N0, q );
+E = qrotate( E0, q );
+U = qrotate( U0, q );
+
+end
