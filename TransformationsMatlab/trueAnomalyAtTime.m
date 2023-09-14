4d9f2515e6aa6f3b6996ffc2cd2b80ea24fecbed
Minwoong Choi <minwoong@usc.edu>
2023-09-14 03:02:18 -0700

    Update trueAnomalyAtTime.m

--- TransformationsMatlab/trueAnomalyAtTime.m
+++ TransformationsMatlab/trueAnomalyAtTime.m
@@ -9,15 +9,15 @@ tsize = size(tSincePeriapsis);
 theta = zeros(tsize);
 
 P = 2 * pi / n; % period of orbit
-t = -P/2 + mod(tSincePeriapsis + P/2, P);
-Ms = 360 * t / P; % Mean anomaly in degrees
+tArray = -P/2 + mod(tSincePeriapsis + P/2, P);
+MArray = 360 * tArray / P; % Mean anomaly in degrees
 
 % Solution of Kepler's equation for time since periapsis
 eStar = e * 180 / pi;
 tol = 1.0e-6; 
 
-for i = 1:length(t)
-    M = Ms(i);
+for i = 1:length(tarray)
+    M = MArray(i);
     
     E = M + e * sind(M);
     DeltaM = 1.0e10;
@@ -27,7 +27,7 @@ for i = 1:length(t)
         E = E + DeltaE;
     end
 
-    cosTheta = (cosd(E) - ecc) / (1 - ecc * cosd(E));
+    cosTheta = (cosd(E) - e) / (1 - e * cosd(E));
     theta(i) = sign(E) * acosd(cosTheta);
 end
 
