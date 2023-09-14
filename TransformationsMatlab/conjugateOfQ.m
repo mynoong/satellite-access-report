e5b2f9456a4beefa45a7e6d7a40f7fb5c9c79b62
Minwoong Choi <minwoong@usc.edu>
2023-09-13 19:57:47 -0700

    Update conjugateOfQ.m

---
+++ TransformationsMatlab/conjugateOfQ.m
@@ -0,0 +1,6 @@
+function conj = conjugateOfQ(q)
+%conjugateOfQ gives the conjugate of quaternion q
+
+conj = [-q(1), -q(2), -q(3), q(4)];
+
+end
--- TransformationsMatlab/qConj.m
+++
@@ -1,6 +0,0 @@
-function conj = qConj(q)
-%qConj gives the conjugate of quaternion q
-
-conj = [-q(1), -q(2), -q(3), q(4)];
-
-end
