69c8db0bb269c20aa97644230c1130d7c1a192ba
Minwoong Choi <minwoong@usc.edu>
2023-09-14 11:30:32 -0700

    Update orbitalPerturbationRates.m

--- TransformationsMatlab/orbitalPerturbationRates.m
+++ TransformationsMatlab/orbitalPerturbationRates.m
@@ -1,18 +1,14 @@
-function [dOmegaByDt, domegaByDt] = orbitalPerturbationRates( mu, R0, J2, a, ecc, inc )
-%ORBITALPERTURBATIONRATES Answer the rates of regression of nodes and of
+function [dOmegaByDt, domegaByDt] = orbitalPerturbationRates(mu, R0, J2, a, e, i)
+%orbitalPerturbationRates gives the rates of regression of nodes and of
 %rotation of apsides, in degrees/sec, for an orbit about a body with
 %gravitational parameter mu (km^3/s^2), equatorial radius R0 (km),
 %zonal coefficient J2, with orbital parameters semimajor axis a (km),
-%eccentricity ecc, and inclination inc (degrees)
+%eccentricity e, and inclination i (degrees).
 
-rad = 180/pi; % To convert radians to degrees
+rad = 180 / pi; % To convert radians to degrees
+n = sqrt(mu / (abs(a))^3);  % Mean motion (rad/s)
 
-n = sqrt( mu / a^3 );  % Mean motion (rad/s)
-
-common = rad * 3*n * J2 * R0^2  / ( 2*a^2 * (1 - ecc^2)^2 );
-
-dOmegaByDt = -common * cosd(inc);
-
-domegaByDt = common * ( 4 - 5*sind(inc)^2 ) / 2;
+dOmegaByDt = -3 * rad * n * J2 * R0^2 * cosd(i) / ( 2 * a^2 * (1 - e^2)^2 ) ;
+domegaByDt = 3 * rad * n * J2 * R0^2 * (4 - 5 * sind(i)^2) / ( 4 * a^2 * (1 - e^2)^2 );
 
 end
