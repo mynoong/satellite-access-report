581ecf65455c93bed1c84e2de3785ba4653958d4
Minwoong Choi <minwoong@usc.edu>
2023-09-14 11:22:51 -0700

    Update ECItoECEF.m

--- TransformationsMatlab/ECItoECEF.m
+++ TransformationsMatlab/ECItoECEF.m
@@ -1,11 +1,10 @@
-function vECEF = ECItoECEF( vI, tJulian )
-%ECItoECEF Convert a vector in Earth-centered inertial (ECI)
+function rECEF = ECItoECEF(rECI, tJulian)
+%ECItoECEF converts a vector in Earth-centered inertial (ECI)
 %coordinates to Earth-centered Earth-fixed (ECEF) coordinates
 %at Julian date tJulian
 
-a = angleOfVernalEquinox( tJulian );
-qECItoECEF = rotationQZ( -a );
-vECEF = qrotate( vI, qECItoECEF );
+alpha = angleOfVernalEquinox(tJulian);
+qECItoECEF = rotationAsQZ(-alpha);
+rECEF = qrotate(rECI, qECItoECEF);
 
 end
-
