90989e231138a05305160d9e474c81836da179cb
Minwoong Choi <minwoong@usc.edu>
2023-09-14 11:38:04 -0700

    Update fromJulianDate.m

--- TransformationsMatlab/fromJulianDate.m
+++ TransformationsMatlab/fromJulianDate.m
@@ -1,14 +1,14 @@
-function [dateString, timeUTHours] = fromJulianDate(julianDate)
+function [dateString, hoursUT] = fromJulianDate(julianDate)
 %fromJulianDate gives the date string and UT time in hours corresponding
 %to a Julian date
 %
 %Example: if julianDate = 2452725.208333, then dateString='26-Mar-2003' and
-%timeUTHours = 17
+%hoursUT = 17
 
 delta = julianDate - 2452724.500000;
 deltaDays = floor( delta );
 dateString = datestr(datenum('26-Mar-2003') + deltaDays);
-timeUTHours = 24 * (delta - deltaDays);
+hoursUT = 24 * (delta - deltaDays);
 
 end
 
