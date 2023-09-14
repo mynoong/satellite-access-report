1e520a5f47f5d84d6b6189679255281dc70bb03f
Minwoong Choi <minwoong@usc.edu>
2023-09-14 11:19:31 -0700

    Update angleOfVernalEquinox.m

--- TransformationsMatlab/angleOfVernalEquinox.m
+++ TransformationsMatlab/angleOfVernalEquinox.m
@@ -1,23 +1,32 @@
-function a = angleOfVernalEquinox( tJulian )
-%ANGLEOFVERNALEQUINOX Answer the angle from the vernal equinox vector
-%(x_i) to the ECEF x-axis at Julian date tJulian
+function alpha = angleOfVernalEquinox(tJulian)
+%angleOfVernalEquinox gives the angle between the vernal equinox vector(x_i)
+%to the ECEF x-axis at Julian date tJulian
 
-siderealDay = physicalConstant( 'siderealDay' );
+siderealDay = physicalConstant('siderealDay');
+earthTilt = physicalConstant('earthTilt');
 
-% t: Time of day (sec) since March 17, 2021, 00:00 UT
-t = ( tJulian - 2459290.500000 ) * 86400;
-timeOfSunSouthing = 12*3600 + 8*60 + 17.2;
-hecLo = 176.8287;
-hecLa = 0.0001;
+% First, set the reference time in which Sun is in the plane of Prime Meridian,
+% ECEF x-axis, so the observer at longitude 0 would see Sun directly to the south.
+% And get the angle between reference time's ECEF x-axis and the fixed vernal
+% equinox vector.
+% reference time: March 17, 2021, 12:08:17.2 UT
 
-vecToEarthHE = [ cosd(hecLa)*cosd(hecLo), cosd(hecLa)*sind(hecLo), sind(hecLa) ];
-HEtoECI = rotationQX( 23.43928 );
-vecToEarth = qrotate( vecToEarthHE, HEtoECI );
-vecToSun = -vecToEarth; % In ECI coords
+% Earth's coordinate in heliocentric ecliptic at the reference time
+HELo = 176.8287; 
+HELa = 0.0001;
 
-% Now angle between x-vectors at noon 
-noonXVectorAngle = atan2d( vecToSun(2), vecToSun(1) );
+rEarthHE = [cosd(HELa) * cosd(HELo), cosd(HELa) * sind(HELo), sind(HELa)];
+rSunHE = -rEarthHE;
+HEtoECI = rotationAsQX(earthTilt);
+rSunECI = qrotate(rSunHE, HEtoECI); % position of Sun in ECI coordinates
+% x-y-plane projected vector of rSunECI is used below for the angle alpharef.
+alpharef = atan2d(rSunECI(2), rSunECI(1));
 
-a = noonXVectorAngle + 360*( t - timeOfSunSouthing )/siderealDay; 
+% Second, add angles from reference time to the given Julian date using
+% the fact that Earth rotates 360 degrees in 1 sidereal day.
+% t: # of seconds since the reference time
+t = (tJulian - 2459291.005755) * 86400;
+
+alpha = alpharef + 360 * t / siderealDay; 
 
 end
