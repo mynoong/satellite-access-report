a0d9a00f0ddd5446665bfb7c8888ae2fec4bb8fb
Minwoong Choi <minwoong@usc.edu>
2023-09-14 22:29:20 -0700

    Update orbitalPerturbationRates.m

--- TransformationsMatlab/orbitalPerturbationRates.m
+++ TransformationsMatlab/orbitalPerturbationRates.m
@@ -1,4 +1,4 @@
-function [dOmegaByDt, domegaByDt] = orbitalPerturbationRates(mu, R0, J2, a, e, i)
+function [dOmegaDt, domegaDt] = orbitalPerturbationRates(mu, R0, J2, a, e, i)
 %orbitalPerturbationRates gives the rates of regression of nodes and of
 %rotation of apsides, in degrees/sec, for an orbit about a body with
 %gravitational parameter mu (km^3/s^2), equatorial radius R0 (km),
@@ -8,7 +8,7 @@ function [dOmegaByDt, domegaByDt] = orbitalPerturbationRates(mu, R0, J2, a, e, i
 rad = 180 / pi; % To convert radians to degrees
 n = sqrt(mu / (abs(a))^3);  % Mean motion (rad/s)
 
-dOmegaByDt = -3 * rad * n * J2 * R0^2 * cosd(i) / ( 2 * a^2 * (1 - e^2)^2 ) ;
-domegaByDt = 3 * rad * n * J2 * R0^2 * (4 - 5 * sind(i)^2) / ( 4 * a^2 * (1 - e^2)^2 );
+dOmegaDt = -3 * rad * n * J2 * R0^2 * cosd(i) / ( 2 * a^2 * (1 - e^2)^2 ) ;
+domegaDt = 3 * rad * n * J2 * R0^2 * (4 - 5 * sind(i)^2) / ( 4 * a^2 * (1 - e^2)^2 );
 
 end
