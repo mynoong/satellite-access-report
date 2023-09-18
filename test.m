5687018f6a29ec6fd5a5bae9ebca78fd9929411c
Minwoong Choi <minwoong@usc.edu>
2023-09-18 13:24:38 -0700

    Update test.m

--- test.m
+++ test.m
@@ -6,6 +6,13 @@ function test()
 % Time: 17-Sep-2023 00:00:00 to 29-Sep-2023 00:00:00,
 %       10 seconds interval, in KST
 
+% Lo: longitude of desired ground station location. (ex. for ground station at N Seoul Tower, Lo = +126.9882)
+% La: latitude of desired ground station location. (ex. for ground station at N Seoul Tower, La = +37.5512)
+% hoursFromUT: time difference between ground station location and UTC. (ex. for ground stations in South Korea, hoursFromUT = +9)
+% tleFilename: TLE file name of desired satellite. You could find several from https://celestrak.org/. (ex. tleFilename = 'iss.tle')
+% txtFilename: txt file name of satellite access report (ex. txtFilename = 'accessReport_iss_Seoul.txt')
+% times: list of julian dates for observation in row vector
+
 Lo = 126.9882
 La = 37.5512
 hoursFromUT = 9
